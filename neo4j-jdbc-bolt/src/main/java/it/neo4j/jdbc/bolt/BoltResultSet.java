f712d9d44f692bea0978593fadf735cc422a9fbc
mfalcier-larus <marco.falcier@larus-ba.it>
2016-02-18 01:22:06 +0100

    added UnsupportedOperationException to Bolt module

--- neo4j-jdbc-bolt/src/main/java/it/neo4j/jdbc/bolt/BoltResultSet.java
+++ neo4j-jdbc-bolt/src/main/java/it/neo4j/jdbc/bolt/BoltResultSet.java
@@ -1,17 +1,19 @@
 /**
- * Copyright (c) 2004-2016 LARUS Business Automation [http://www.larus-ba.it]
+ * Copyright (c) 2016 LARUS Business Automation [http://www.larus-ba.it]
  * <p>
  * This file is part of the "LARUS Integration Framework for Neo4j".
  * <p>
- * The "LARUS Integration Framework for Neo4j" is free software: you can
- * redistribute it and/or modify it under the terms of the GNU General Public
- * License as published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * The "LARUS Integration Framework for Neo4j" is licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  * <p>
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  * <p>
  * Created on 11/02/16
  */
@@ -64,11 +66,11 @@ public class BoltResultSet extends ResultSet {
 	}
 
 	@Override public boolean first() throws SQLException {
-		return false;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override public boolean last() throws SQLException {
-		return false;
+		throw new UnsupportedOperationException();
 	}
 
 }
