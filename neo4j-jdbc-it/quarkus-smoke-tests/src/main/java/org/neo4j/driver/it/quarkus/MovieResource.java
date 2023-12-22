2b312cc682dac841efb543f9e39fe5580c8f6776
Michael Simons <michael.simons@neo4j.com>
2023-12-22 14:34:21 +0100

    build: Add a Quarkus smoke test.
    Utilizing Agroal connection pool, javax.sql.Datasources and Jdbi.
    
    Closes #113.

---
+++ neo4j-jdbc-it/quarkus-smoke-tests/src/main/java/org/neo4j/driver/it/quarkus/MovieResource.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2023 "Neo4j,"
+ * Neo4j Sweden AB [https://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.neo4j.driver.it.quarkus;
+
+import java.util.List;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.jdbi.v3.core.Jdbi;
+
+@Path("/movies")
+public class MovieResource {
+
+	private final Jdbi db;
+
+	public MovieResource(Jdbi db) {
+		this.db = db;
+	}
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Movie> get() {
+
+		return this.db
+			.withHandle(handle -> handle.createQuery("SELECT m.title AS title FROM Movie m").mapTo(Movie.class).list());
+	}
+
+}
