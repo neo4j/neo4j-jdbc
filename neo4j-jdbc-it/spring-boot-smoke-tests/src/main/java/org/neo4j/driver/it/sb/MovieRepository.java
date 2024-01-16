9dbefcfe9c12d3aef3b61154d3765b10ab479d3e
Michael Simons <michael.simons@neo4j.com>
2024-01-16 08:15:27 +0100

    build: Add a Spring JDBCTemplate based set of integration tests.

---
+++ neo4j-jdbc-it/spring-boot-smoke-tests/src/main/java/org/neo4j/driver/it/sb/MovieRepository.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2023-2024 "Neo4j,"
+ * Neo4j Sweden AB [https://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.neo4j.driver.it.sb;
+
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
+public class MovieRepository {
+
+	private final JdbcTemplate jdbcTemplate;
+
+	public MovieRepository(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	@Transactional(readOnly = true)
+	public List<Movie> findAll() {
+		return this.jdbcTemplate.query("SELECT m.title AS title FROM Movie m",
+				(rs, rowNum) -> new Movie(rs.getString("title")));
+	}
+
+	public int createOrUpdate(Movie movie) {
+		return this.jdbcTemplate.update("INSERT INTO Movie(title) VALUES(?) ON DUPLICATE KEY IGNORE", movie.title());
+	}
+
+}
---
+++ neo4j-jdbc-it/spring-boot-smoke-tests/src/test/java/org/neo4j/driver/it/sb/Neo4jTestConfig.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2023-2024 "Neo4j,"
+ * Neo4j Sweden AB [https://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.neo4j.driver.it.sb;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
+import org.testcontainers.containers.wait.strategy.WaitStrategy;
+import org.testcontainers.utility.DockerImageName;
+
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+
+@TestConfiguration(proxyBeanMethods = false)
+@ImportAutoConfiguration(JacksonAutoConfiguration.class)
+class Neo4jTestConfig {
+
+	@Bean
+	@ServiceConnection
+	Neo4jJdbcContainer<?> neo4jContainer() {
+		return new Neo4jJdbcContainer<>(System.getProperty("neo4j-jdbc.default-neo4j-image")).withReuse(true);
+	}
+
+	@Bean
+	Neo4jHttpClient neo4jHttpClient(ObjectMapper objectMapper, Neo4jJdbcContainer<?> neo4jJdbcContainer) {
+		return new Neo4jHttpClient(objectMapper,
+				"http://%s:%d".formatted(neo4jJdbcContainer.getHost(), neo4jJdbcContainer.getMappedPort(7474)),
+				neo4jJdbcContainer.adminPassword);
+	}
+
+	static class Neo4jJdbcContainer<S extends Neo4jJdbcContainer<S>> extends JdbcDatabaseContainer<S> {
+
+		private static final WaitStrategy WAIT_FOR_BOLT = (new LogMessageWaitStrategy())
+			.withRegEx(String.format(".*Bolt enabled on .*:%d\\.\n", 7687));
+
+		final String adminPassword = "verysecret";
+
+		Neo4jJdbcContainer(String dockerImageName) {
+			super(DockerImageName.parse(dockerImageName));
+			this.addEnv("NEO4J_ACCEPT_LICENSE_AGREEMENT", "yes");
+			this.waitStrategy = WAIT_FOR_BOLT;
+			this.addExposedPorts(7687, 7474, 7473);
+		}
+
+		protected void configure() {
+			this.addEnv("NEO4J_AUTH", "neo4j/%s".formatted(this.adminPassword));
+		}
+
+		@Override
+		public String getDriverClassName() {
+			return "org.neo4j.driver.jdbc.Neo4jDriver";
+		}
+
+		@Override
+		public String getJdbcUrl() {
+			return "jdbc:neo4j://%s:%d?sql2cypher=true".formatted(this.getHost(), this.getMappedPort(7687));
+		}
+
+		@Override
+		public String getUsername() {
+			return "neo4j";
+		}
+
+		@Override
+		public String getPassword() {
+			return this.adminPassword;
+		}
+
+		@Override
+		protected String getTestQueryString() {
+			return "/*+ NEO4J FORCE_CYPHER */ RETURN 1";
+		}
+
+	}
+
+}
