3cb81a03c0c44e9ee33c2e336d16f5dba8048ddd
Michael Simons <michael.simons@neo4j.com>
2025-11-11 17:02:25 +0100

    chore: Update dependencies for Antora-Documentation generation, bump driver version in `package.json`.
    This change also enhances the release script to bump the driver version in `package.json` during release.
    
    Signed-off-by: Michael Simons <michael.simons@neo4j.com>

--- bin/prepare-release.sh
+++ bin/prepare-release.sh
@@ -23,8 +23,10 @@ set -euo pipefail
 DIR="$(dirname "$(realpath "$0")")"
 
 sed -i.bak 's/\(:latest_version:\) \(.*\)/\1 '"${1}"'/g' $DIR/../README.adoc
-
 rm $DIR/../README.adoc.bak
+
+sed -i.bak 's/\(.*"version":\) ".*",/\1 "'${1}'",/g' $DIR/../etc/antora/package.json
+rm $DIR/../etc/antora/package.json.bak
 
 if test -n "${2-}"; then
   DRYRUN=$2
